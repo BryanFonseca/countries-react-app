8b0b73f14ecb76c7be964b4552c7dce303f4ca1c
Bryan Fonseca <isaac1805@hotmail.com>
2022-05-01 19:16:44 -0500

    Add loading cards in chunks to improve overall performance

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import AppContext from "../../context/app-context";
 import classes from "./Countries.module.css";
 import CountryCard from "./CountryCard";
@@ -21,7 +21,76 @@ const Countries = () => {
     }
   }, [ctx.setCountriesData, request, ctx.countriesData]);
 
-  let countriesCards;
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (!ctx.countriesData) return;
+
+    const dataSource = ctx.filteredCountries ?? ctx.countriesData;
+
+    if(ctx.filteredCountries) {
+      setCards([]);
+    }
+
+    const cardsCount = dataSource.length;
+    const divideInto = Math.ceil(cardsCount / 20);
+    //const divideInto = 10; 
+    const chunkSize = Math.floor(cardsCount / divideInto);
+    let iteration = 0;
+
+    setTimeout(function generateCards() {
+      const base = chunkSize * iteration;
+      const toAddCardsArray = [];
+      // from  base to (base + chunkSize) per iteration
+      for (let i = base; i < base + chunkSize; i++) {
+	const card = <NavLink
+            key={dataSource[i].name.common}
+            to={`/countries/${dataSource[i].name.common}`}
+          >
+            <CountryCard
+              imageUrl={dataSource[i].flags?.png ?? dataSource[i].flags?.svg}
+              name={dataSource[i].name.common}
+              population={dataSource[i].population}
+              region={dataSource[i].region}
+              capital={dataSource[i]?.capital?.[0] ?? dataSource[i].capital ?? ""}
+            />
+          </NavLink>
+	toAddCardsArray.push(card)
+      }
+      setCards((prevState) => {
+	return [
+	  ...prevState,
+	  ...toAddCardsArray
+	];
+      })
+      iteration++;
+      if (iteration < divideInto) {
+	setTimeout(generateCards, 0);
+      }
+    }, 0);
+
+/*     setCards(() => {
+      return dataSource.map((countryData) => {
+        return (
+          <NavLink
+            key={countryData.name.common}
+            to={`/countries/${countryData.name.common}`}
+          >
+            <CountryCard
+              imageUrl={countryData.flags?.png ?? countryData.flags?.svg}
+              name={countryData.name.common}
+              population={countryData.population}
+              region={countryData.region}
+              capital={countryData?.capital?.[0] ?? countryData.capital ?? ""}
+            />
+          </NavLink>
+        );
+      });
+    }); */
+  }, [isLoading, ctx.filteredCountries, ctx.countriesData]);
+
+  /*   let countriesCards;
   if (ctx.countriesData) {
     const dataSource = ctx.filteredCountries ?? ctx.countriesData;
     countriesCards = dataSource.map((countryData) => {
@@ -40,12 +109,12 @@ const Countries = () => {
         </NavLink>
       );
     });
-  }
+  } */
 
   return (
     <ul className={classes.countriesGrid}>
       {error}
-      {isLoading ? <p>Loading...</p> : countriesCards}
+      {isLoading ? <p>Loading...</p> : cards}
     </ul>
   );
 };
